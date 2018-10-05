96c3f977d76f91cd39f6c2624c8dceb4e565feff
Max Gefvert <max.gefvert@qlik.com>
2018-10-05 08:28:26 +0200

    calculator done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { Keypad } from './Keypad';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: '',
+      expression: '',
+      isEvaluated: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick(keyValue) {
+    switch (keyValue) {
+      case '=':
+        let result;
+
+        try {
+          result = eval(this.state.expression);
+        } catch (error) {
+          result = 'SYNTAX ERROR';
+        }
+
+        this.setState({
+          isEvaluated: true,
+          expression: '',
+          answer: result
+        });
+        break;
+
+      case 'AC':
+        this.setState({
+          isEvaluated: false,
+          expression: ''
+        });
+        break;
+
+      case 'ans':
+        if(this.state.answer !== 'SYNTAX ERROR') {
+          this.setState({
+            isEvaluated: false,
+            expression: this.state.expression + this.state.answer
+          });
+        }
+       
+        break;
+
+      case 'x':
+        this.setState({
+          isEvaluated: false,
+          expression: this.state.expression + '*'
+        });
+        break;
+    
+      default:
+        this.setState({
+          isEvaluated: false,
+          expression: this.state.expression + keyValue
+        });
+        break;
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="outputWindow">
+          {this.state.isEvaluated ? this.state.answer : this.state.expression}
+        </div>
+        <Keypad onClick={(keyValue) => this.handleClick(keyValue)} />
+        <br/>
       </div>
     );
   }
---
+++ src/Keypad.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const keyValues = [
+  'ans', '(', ')', '/',
+  '7','8','9','x',
+  '6','5','4','-',
+  '3','2','1','+',
+  'AC','0','.','=',
+];
+
+export class Key extends Component {
+  render() {
+    return (
+      <button className="box" onClick={ () => this.props.onClick(this.props.value) }>{ this.props.value }</button>
+    );
+  }
+}
+
+export class Keypad extends Component {
+  render() {
+    let keyElementArray = []
+    for (var i = 0; i < 20; ++i) {
+      keyElementArray.push(
+      <Key key={'key-' + i} value={ keyValues[i] } onClick={ (keyValue) => this.props.onClick(keyValue) } />);
+    } 
+
+    return (
+      <div>
+        {keyElementArray.slice(0,4)}
+        <br/>
+        {keyElementArray.slice(4,8)}
+        <br/>
+        {keyElementArray.slice(8,12)}
+        <br/>
+        {keyElementArray.slice(12,16)}
+        <br/>
+        {keyElementArray.slice(16,20)}
+        <br/>
+
+      </div>
+      
+    );
+  }
+}
